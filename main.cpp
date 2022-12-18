477dc43013dc6c6153ef9a3f9d5c3e61a1af2b21
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2022-12-18 20:18:46 +0300

    Add Arabic translations to the application

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
+#include <QTranslator>
 
 #include "windows/main_window.h"
 
 int main(int argc, char *argv[]) {
   QApplication qApplication(argc, argv);
+
+  QTranslator qtTranslator;
+  if (qtTranslator.load(":/qt_ar.qm")) {
+    qApplication.installTranslator(&qtTranslator);
+  }
 
   MainWindow mainWindow;
   mainWindow.show();
