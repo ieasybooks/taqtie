d71a905d46d4b2c2ef57d044d0a46815205bcf2d
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2023-01-01 13:13:29 +0300

    Add application properties to main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 int main(int argc, char *argv[]) {
   QApplication qApplication(argc, argv);
 
+  qApplication.setOrganizationName("الكتب المُيسّرة");
+  qApplication.setOrganizationDomain("ieasybooks.com");
+  qApplication.setApplicationDisplayName("تقطيع");
+  qApplication.setApplicationName("taqtie");
+  qApplication.setApplicationVersion("0.1");
+
   QTranslator qtTranslator;
   if (qtTranslator.load(":/qt_ar.qm")) {
     qApplication.installTranslator(&qtTranslator);
